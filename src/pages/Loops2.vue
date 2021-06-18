812ac7627172b809dd2c985a6cd532e6edc8d9f0
Ayuagustin <agustinayu818@gmail.com>
2021-06-18 15:38:09 +0700

    Tugas kalkulator

---
+++ src/pages/Loops2.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+      return{
+          nama : "Ayu agustin",
+          tahunlahir : 1998,
+          kotalahir : "Tangerang"
+      }  
+    },
+    methods:{
+        next(){
+            this.tahunlahir += 1;
+        }
+    },
+    mounted(){
+        this.tahunlahir = 2021 - this.tahunlahir;
+    }
+}
+</script>
